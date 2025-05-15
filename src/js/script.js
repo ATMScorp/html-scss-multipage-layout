473747d11c8f8d52b084bfbdf0bcae6cd5e1fa53
Aleksander Truchanowicz <alextrukhanovich034@gmail.com>
2025-05-15 21:47:20 +0200

    feat: add client-side validation using JustValidate

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
+import JustValidate from 'just-validate';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -55,18 +56,92 @@ try {
 
     tabs.forEach((tab, index) => {
         tab.addEventListener("click", () => {
-            // Удаляем активный класс у всех табов и контента
             tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
             contents.forEach((c) => (c.style.display = "none"));
 
-            // Добавляем активный класс к нажатому табу и показываем соответствующий контент
             tab.classList.add("catalog__tab_active");
             contents[index].style.display = "flex";
         });
     });
 
-    // Показываем первый контент при загрузке
     contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
 } catch (e) { }
 
-// Обратите внимание, что значение block (в двух местах) можно спокойно поменять на flex, если вам это необходимо
+try {
+    const validatorTouch = new JustValidate('.touch__form');
+
+
+    validatorTouch
+        .addField('#name', [
+            {
+                rule: 'required',
+                errorMessage: 'Please fill the name',
+            },
+            {
+                rule: 'minLength',
+                value: 2,
+                errorMessage: 'Too short!',
+            },
+        ])
+        .addField('#email', [
+            {
+                rule: 'required',
+            },
+            {
+                rule: 'email',
+            },
+        ])
+        .addField('#question', [
+            {
+                rule: 'required',
+            },
+            {
+                rule: 'minLength',
+                value: 5,
+            },
+        ], {
+            errorsContainer: document.querySelector('#question').parentElement.parentElement.querySelector('.error-message'),
+        })
+        .addField('#checkbox', [
+            {
+                rule: 'required',
+            }
+        ], {
+            errorsContainer: document.querySelector('#checkbox').parentElement.parentElement.querySelector('.checkbox-error-message'),
+        });
+} catch (e) { }
+
+try {
+    const validatorFooter = new JustValidate(".footer__form");
+
+    validatorFooter
+        .addField(
+            "#footer__email",
+            [
+                {
+                    rule: "required",
+                },
+                {
+                    rule: "email",
+                },
+            ],
+            {
+                errorsContainer: document
+                    .querySelector("#footer__email")
+                    .parentElement.querySelector(".email-error-message"),
+            }
+        )
+        .addField(
+            "#footer__checkbox",
+            [
+                {
+                    rule: "required",
+                },
+            ],
+            {
+                errorsContainer: document
+                    .querySelector("#footer__checkbox")
+                    .parentElement.parentElement.querySelector(".check-error-message"),
+            }
+        );
+} catch (e) { }
